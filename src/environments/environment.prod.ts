1e9edf804166df0d891e2214cea121d5a4702874
Monur <monurakkaya@gmail.com>
2021-06-13 22:01:38 +0300

    environment update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   endpoint: 'https://smarty.kerzz.com:4004/api',
-  apiKey: 'bW9jay04ODc3NTU2NjExMjEyNGZmZmZmZmJ2'
+  apiKey: 'ENTER_API_KEY_HERE'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   endpoint: 'https://smarty.kerzz.com:4004/api',
-  apiKey: 'bW9jay04ODc3NTU2NjExMjEyNGZmZmZmZmJ2'
+  apiKey: 'ENTER_API_KEY_HERE'
 };
 
 /*
